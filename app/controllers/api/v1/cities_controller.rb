0fb04c4d4bc1ee406c589adcccf841633a7e022c
Carlos Salgado <carlosalgado@MacBook-Pro-de-Carlos.local>
2023-12-18 19:39:00 -0600

    Fix: order and clean code

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,8 +1,14 @@
+require 'active_support/core_ext/string'
 require 'httparty'
 require 'json'
 
 class Api::V1::CitiesController < ApplicationController
   before_action :validate_params, only: [:weather]
+
+  def initialize
+    super
+    @only_mexico_cities = (ENV.fetch("ONLY_MEXICO_CITIES") { 'true' }) == 'true'
+  end
 
   def weather
     begin
@@ -16,52 +22,16 @@ class Api::V1::CitiesController < ApplicationController
         weather_cities = []
 
         cities.each do |city|
-          if ENV.fetch("ONLY_MEXICO_CITIES") == 'true'
-            if city["country"] == 'México'
-              weather_url = "#{ENV.fetch("WEATHER_API")}?lat=#{city['lat']}&lon=#{city['long']}&units=metric&appid=#{ENV.fetch("WEATHER_API_KEY")}"
-              weather_request = HTTParty.get(weather_url)
-
-              if weather_request.code == 200
-                weather_city = []
-                weather_forecast = JSON.parse(weather_request.body)
-                weather_forecast_daily = weather_forecast['list'].select { |item| item['dt_txt'].include?('09:00:00') }
-                weather_forecast_daily.each do |weather_forecast_day|
-                  weather_city << {
-                    "day": weather_forecast_day['dt_txt'],
-                    "min": "#{weather_forecast_day['main']['temp_min']}°C",
-                    "max": "#{weather_forecast_day['main']['temp_max']}°C"
-                  }
-                end
-                weather_cities << {
-                  "city": city['display'],
-                  "country": city['country'],
-                  "weathers": weather_city
-                }
-              end
-            end
-          else
-            weather_url = "#{ENV.fetch("WEATHER_API")}?lat=#{city['lat']}&lon=#{city['long']}&units=metric&appid=#{ENV.fetch("WEATHER_API_KEY")}"
-            weather_request = HTTParty.get(weather_url)
-
-            if weather_request.code == 200
-              weather_city = []
-              weather_forecast = JSON.parse(weather_request.body)
-              weather_forecast_daily = weather_forecast['list'].select { |item| item['dt_txt'].include?('09:00:00') }
-              weather_forecast_daily.each do |weather_forecast_day|
-                weather_city << {
-                  "day": weather_forecast_day['dt_txt'],
-                  "min": "#{weather_forecast_day['main']['temp_min']}°C",
-                  "max": "#{weather_forecast_day['main']['temp_max']}°C"
-                }
-              end
-              weather_cities << {
-                "city": city['display'],
-                "country": city['country'],
-                "weathers": weather_city
-              }
-            end
+          if (@only_mexico_cities && city["country"] == 'México') || !@only_mexico_cities
+            weather_city = get_waether_with_lat_long(city['lat'], city['long'])
+            weather_cities << {
+              "city": city['display'],
+              "country": city['country'],
+              "weathers": weather_city
+            }
           end
         end
+
         render json: {"data": weather_cities}, status: 201
       end
     rescue StandardError => e
@@ -78,5 +48,26 @@ class Api::V1::CitiesController < ApplicationController
     if param_cities.nil? || param_cities.empty?
       render json: { error: "El parámetro 'q' no está presente o está vacío" }, status: :bad_request
     end
+  end
+
+  private
+  def get_waether_with_lat_long(lat, long)
+    weather_city = []
+    weather_url = "#{ENV.fetch("WEATHER_API")}?lat=#{lat}&lon=#{long}&units=metric&appid=#{ENV.fetch("WEATHER_API_KEY")}"
+    weather_request = HTTParty.get(weather_url)
+
+    if weather_request.code == 200
+      weather_forecast = JSON.parse(weather_request.body)
+      weather_forecast_daily = weather_forecast['list'].select { |item| item['dt_txt'].include?('09:00:00') }
+      weather_forecast_daily.each do |weather_forecast_day|
+        weather_city << {
+          "date": weather_forecast_day['dt_txt'].gsub(' 09:00:00', ''),
+          "min": "#{weather_forecast_day['main']['temp_min']}°C",
+          "max": "#{weather_forecast_day['main']['temp_max']}°C"
+        }
+      end
+    end
+
+    return weather_city
   end
 end
